6646402d635dc50b2ce836f512870039bb60cafa
0xantman <antho74830@gmail.com>
2021-12-15 19:22:28 +0100

    Fix to 6 decimals

--- src/components/SingleNft/BiddingInterface.jsx
+++ src/components/SingleNft/BiddingInterface.jsx
@@ -26,7 +26,7 @@ export default function BiddingInterface(props) {
 
     function format_time(s) {
         return new Date(s * 1e3).toISOString().slice(-13, -5);
-      }
+    }
 
 let bootstrap = {}
 if (typeof document !== 'undefined') {
@@ -111,15 +111,15 @@ if (typeof document !== 'undefined') {
                                         {nftData.highest_bid != bidder.total_bid && nftData.highest_bid &&
                                         <p 
                                         style={{textDecoration:'underline', fontSize:'12px', fontWeight:300}} 
-                                        onClick={() => setAmount(nftData.highest_bid ? (((parseInt(nftData.highest_bid) + (parseInt(nftData.highest_bid) * 5 / 100)) - parseInt(bidder.total_bid)) / 1000000).toFixed(2) : nftData.highest_bid === null && nftData.start_price !== null ? nftData.start_price / 1000000 : 0)}>
-                                            Add minimal {nftData.highest_bid ? (((parseInt(nftData.highest_bid) + (parseInt(nftData.highest_bid) * 5 / 100)) - parseInt(bidder.total_bid)) / 1000000).toFixed(2) : nftData.highest_bid === null && nftData.start_price !== null ? nftData.start_price / 1000000 : 0} UST
+                                        onClick={() => setAmount(nftData.highest_bid ? (((parseInt(nftData.highest_bid) + (parseInt(nftData.highest_bid) * 5 / 100)) - parseInt(bidder.total_bid)) / 1000000).toFixed(6) : nftData.highest_bid === null && nftData.start_price !== null ? nftData.start_price / 1000000 : 0)}>
+                                            Add minimal {nftData.highest_bid ? (((parseInt(nftData.highest_bid) + (parseInt(nftData.highest_bid) * 5 / 100)) - parseInt(bidder.total_bid)) / 1000000).toFixed(6) : nftData.highest_bid === null && nftData.start_price !== null ? nftData.start_price / 1000000 : 0} UST
                                         </p>
                                         }   
                                         {!parseInt(nftData.highest_bid) > 0 && parseInt(nftData.start_price) > 0 &&
                                         <p 
                                         style={{textDecoration:'underline', fontSize:'12px', fontWeight:300}} 
-                                        onClick={() => setAmount(nftData.highest_bid ? (((parseInt(nftData.highest_bid) + (parseInt(nftData.highest_bid) * 5 / 100)) - parseInt(bidder.total_bid)) / 1000000).toFixed(2) : nftData.highest_bid === null && nftData.start_price !== null ? nftData.start_price / 1000000 : 0)}>
-                                            Add minimal {nftData.highest_bid ? (((parseInt(nftData.highest_bid) + (parseInt(nftData.highest_bid) * 5 / 100)) - parseInt(bidder.total_bid)) / 1000000).toFixed(2) : nftData.highest_bid === null && nftData.start_price !== null ? nftData.start_price / 1000000 : 0} UST
+                                        onClick={() => setAmount(nftData.highest_bid ? (((parseInt(nftData.highest_bid) + (parseInt(nftData.highest_bid) * 5 / 100)) - parseInt(bidder.total_bid)) / 1000000).toFixed(6) : nftData.highest_bid === null && nftData.start_price !== null ? nftData.start_price / 1000000 : 0)}>
+                                            Add minimal {nftData.highest_bid ? (((parseInt(nftData.highest_bid) + (parseInt(nftData.highest_bid) * 5 / 100)) - parseInt(bidder.total_bid)) / 1000000).toFixed(6) : nftData.highest_bid === null && nftData.start_price !== null ? nftData.start_price / 1000000 : 0} UST
                                         </p>
                                         } 
                                         </div>
