50c20db77af8c7252129dbd023f1b768d33ca2bb
0xantman <antho74830@gmail.com>
2021-12-23 18:19:56 +0100

    Add Tales of Terra in contract json

--- src/components/NftCard.jsx
+++ src/components/NftCard.jsx
@@ -50,7 +50,7 @@ export default function NftCard(props) {
                                 <img src={"https://ipfs.io/ipfs/" +data.extension.image.split("/").pop()} className="card-img" />
                             )}
                             {data.extension && data.extension.animation_url && (                                
-                                <video playsinline="" autoplay="" loop="" muted src={"https://ipfs.io/ipfs/" +data.extension.animation_url.split("/").pop()}></video>
+                                <video playsinline="" autoplay="" loop muted src={"https://ipfs.io/ipfs/" +data.extension.animation_url.split("/").pop()}></video>
                             )}
 
                         <div className="card-img-overlay">
