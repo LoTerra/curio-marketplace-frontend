ff8dfce4267a4f168051b3bf7ac1caf72748ec41
0xantman <antho74830@gmail.com>
2022-01-19 20:45:36 +0100

    Fix

--- src/pages/Nfts/SingleNft.js
+++ src/pages/Nfts/SingleNft.js
@@ -333,9 +333,9 @@ export default (props) => {
 
     function getRawAmountToUnlock() {
         if (nftData.private_sale) {
-            if (parseFloat(nftData.highest_bid) > 0) {
+            if (parseInt(nftData.highest_bid) > 0) {
                 let bid = 1000000
-                let add_to_bid = (parseFloat(nftData.highest_bid) / 100) * 2
+                let add_to_bid = (parseInt(nftData.highest_bid) / 100) * 2
 
                 return bid + add_to_bid
             } else {
