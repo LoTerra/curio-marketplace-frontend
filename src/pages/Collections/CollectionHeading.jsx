c88c444d818055c46d846ede7a0be0323401af1f
0xantman <antho74830@gmail.com>
2022-02-09 14:32:28 +0100

    Fix

--- src/pages/Collections/CollectionHeading.jsx
+++ src/pages/Collections/CollectionHeading.jsx
@@ -106,7 +106,7 @@ export default function CollectionHeading(props) {
     },[])
 
     return (
-        <>
+        <>{contractAddress && auctionStats && (
             <Head>
                 <meta charSet="UTF-8" />
                 <title>{contract.mainnet[0][contractAddress]['name']} | {contract.mainnet[0][contractAddress]["symbol"]}</title>
@@ -126,6 +126,7 @@ export default function CollectionHeading(props) {
                 />
                 <meta property="twitter:description" content="Hey! Take a look at this NFT collection on Curio!" />
             </Head>
+        )}
             {contractAddress && auctionStats && (
                 <div className="collection-verification">                   
                         {verifyAddress(contractAddress)}               
